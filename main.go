389492a190cc4ddcf4e0f8858053edd63991e793
jackie.feng <jackie.feng@liulishuo.com>
2019-05-23 17:24:42 +0800

    Feat(OpCode): decode op_code

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"luago/binchunk"
+	"luago/vm"
 	"os"
 )
 
@@ -50,7 +51,47 @@ func printCode(f *binchunk.Prototype) {
 		if len(f.LineInfo) > 0 {
 			line = fmt.Sprintf("%d", f.LineInfo[pc])
 		}
-		fmt.Printf("\t%d\t[%s]\t0x%08X\n", pc+1, line, c)
+
+		i := vm.Instruction(c)
+		fmt.Printf("\t%d\t[%s]\t%s\t", pc+1, line, i.OpName())
+		printOperands(i)
+		fmt.Printf("\n")
+	}
+}
+
+func printOperands(i vm.Instruction) {
+	switch i.OpMode() {
+	case vm.IABC:
+		a, b, c := i.ABC()
+		fmt.Printf("%d", a)
+		if i.BMode() != vm.OpArgN {
+			if b > 0xFF {
+				fmt.Printf(" %d", -1-b&0xFF)
+			} else {
+				fmt.Printf(" %d", b)
+			}
+		}
+		if i.CMode() != vm.OpArgN {
+			if c > 0xFF {
+				fmt.Printf(" %d", -1-c&0xFF)
+			} else {
+				fmt.Printf(" %d", c)
+			}
+		}
+	case vm.IABx:
+		a, bx := i.ABx()
+		fmt.Printf("%d", a)
+		if i.BMode() == vm.OpArgK {
+			fmt.Printf(" %d", -1-bx)
+		} else if i.BMode() == vm.OpArgU {
+			fmt.Printf(" %d", bx)
+		}
+	case vm.IAsBx:
+		a, sbx := i.AsBx()
+		fmt.Printf("%d %d", a, sbx)
+	case vm.IAx:
+		ax := i.Ax()
+		fmt.Printf("%d", -1-ax)
 	}
 }
 
---
+++ vm/instruction.go
@@ -0,0 +1,62 @@
+package vm
+
+// MaxArgBX BX 无符号数最大值
+const MaxArgBX = 1<<18 - 1
+
+// MaxArgsBx BX 无符号数最大值一半
+const MaxArgsBx = MaxArgBX >> 1
+
+// Instruction 指令
+type Instruction uint32
+
+// Opcode 返回指令码
+func (i Instruction) Opcode() int {
+	return int(i & 0x3F)
+}
+
+// ABC return a,b,c for IABC
+func (i Instruction) ABC() (a, b, c int) {
+	a = int(i >> 6 & 0xFF)
+	c = int(i >> 14 & 0x1FF)
+	b = int(i >> 23 & 0x1FF)
+	return
+}
+
+// ABx return a, bx for IAbx
+func (i Instruction) ABx() (a, bx int) {
+	a = int(i >> 6 & 0xFF)
+	bx = int(i >> 14)
+	return
+}
+
+// AsBx return a, sbx for IAsBx
+func (i Instruction) AsBx() (a, sbx int) {
+	a, bx := i.ABx()
+	return a, bx - MaxArgsBx
+}
+
+// Ax return ax for IAx
+func (i Instruction) Ax() int {
+	return int(i >> 6)
+}
+
+// OpName 返回指令名
+func (i Instruction) OpName() string {
+	return opcodes[i.Opcode()].name
+}
+
+// OpMode 返回指令模式
+func (i Instruction) OpMode() byte {
+	return opcodes[i.Opcode()].opMode
+}
+
+// BMode 返回B操作数类型
+func (i Instruction) BMode() byte {
+	return opcodes[i.Opcode()].argBMode
+}
+
+// CMode 返回C操作数类型
+func (i Instruction) CMode() byte {
+	return opcodes[i.Opcode()].argCMode
+
+}
