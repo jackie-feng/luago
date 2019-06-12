2f5dbd5e97c868d86866d3df3c7f36964fe2f17b
FengJiacheng <hmzxfjc@163.com>
2019-06-12 22:01:00 +0800

    WIP

---
+++ api/lua_state.go
@@ -0,0 +1,30 @@
+package api
+
+type LuaType = int
+
+type LuaState interface {
+	/* Basic stack manipulation */
+	GetTop() int
+	AbsIndex(idx int) int
+	CheckStack(n int) bool
+	Pop(n int)
+	Copy(fromIdx, toIdx int)
+	PushValue(idx int)
+	Replace(idx int)
+	Insert(idx int)
+	Remove(idx int)
+	Rotate(idx int)
+	SetTop(idx int)
+	/* access functions stack->go */
+	TypeName(tp LuaType) string
+	Type(idx int) LuaType
+	IsNone(idx int) bool
+	IsNil(idx int) bool
+	IsNoneOrNil(idx int) bool
+	IsBoolean(idx int) bool
+	IsInteger(idx int) bool
+	IsNumber(idx int) bool
+	IsString(idx int) bool
+	ToBoolean(idx int) bool
+	ToInteger(idx int) int64
+}
